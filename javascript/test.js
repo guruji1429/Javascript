9d60baeaa0dac884ff2b9fbb67d5b716b1e468bc
Gurmeet <singhgurmeet1429@gmail.com>
2025-03-31 06:44:03 +0000

    new

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,19 @@
 console.log("Gurmeet")
+let a=12
+let name1 ="gurmeet"
+let roll =289
+console.table([a,name1,roll])
+
+
+//DATA TYPES
+//numer
+//bigint
+// string
+//null
+//undefined
+//symbol= unique
+//Boolean
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
