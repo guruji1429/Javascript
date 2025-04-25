cc8b1e64bdfa7cf276300434e879c6c64e455ef8
Gurmeet <singhgurmeet1429@gmail.com>
2025-04-25 06:03:52 +0000

    new

--- javascript/test.js
+++ javascript/test.js
@@ -6,8 +6,8 @@ console.table([a,name1,roll])
 
 
 //DATA TYPES
-//numer
-//bigint
+//number  no float type data decimals fall in number too
+//bigint                           
 // string
 //null
 //undefined
@@ -17,3 +17,39 @@ console.table([a,name1,roll])
 console.log(typeof null) //object
 
 console.log(typeof undefined) //undefined
+
+
+
+//type conversion
+//let variablename=datatype(variable to be converted)
+//when we check a string converted to number it gives NaN
+//When we convert null to number it gives 0 
+//when boolean =true=1,false=0
+//when gurmeet=NaN(Not a Number) but its type is number
+
+
+
+//************************************OPERATIONS****************************************
+
+let value=3
+let negvalue=-value
+
+console.log(negvalue); //-3
+
+let str1="Hello"
+let str2=" Guru"
+let str3=str1+ str2
+console.log(str3);
+ 
+let num1,num2,num3
+num1=num2=num3=2+2 //not preferred
+// console.log(num2)
+
+
+//************************************Comparison of datatype****************************
+//strict check (===) it checks both value and datatype
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack (ALL primitive datatypes ) , Heap (Non-Primitive)
+
+    
